467a403a4ef52974a17afa54b7ba25e636fc8525
CcyicC <31849966+CcyicC@users.noreply.github.com>
2017-09-11 20:43:06 +0800

    xatusec
    web-test

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<title>Hello World</title>
+<?php
+/*
+     xatusec
+	 
+	 flag{xatu-github...}
+
+
+*/
+echo "<center>";
+echo "Hello World".'<br>';
+echo "</center>";
+?>
+<center>
+<h3>
+<a href="xatusec.php">xatusec成员名单</a>
+<a href="webg.php">web狗的专属地带</a>
+<a href="pwng.php">pwn大佬</a>
+<a href="miscg.php">misc大佬</a>
+<h3>
+<center>
+</body>
+</html>
